9677b181c93e698aab68c9a3f877dc0a23e0e55f
Nickolay <dobrotvorskiynickolay@gmail.com>
2025-11-03 00:38:16 +0300

    Delete sync imports

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select, update
-from sqlalchemy.orm import Session
 from typing import List
 
 from app.models.products import Product as ProductModel
 from app.models.categories import Category as CategoryModel
 from app.schemas import Product as ProductSchema, ProductCreate
-from app.db_depends import get_db
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db_depends import get_async_db
@@ -79,7 +77,7 @@ async def get_product(product_id: int, db: AsyncSession = Depends(get_async_db))
     Возвращает детальную информацию о товаре по его ID.
     """
     # Проверяем, существует ли активный товар
-    temp = db.scalars(
+    temp = await db.scalars(
         select(ProductModel).where(ProductModel.id == product_id, ProductModel.is_active == True)
     )
     product = temp.first()
@@ -91,7 +89,7 @@ async def get_product(product_id: int, db: AsyncSession = Depends(get_async_db))
         select(CategoryModel).where(CategoryModel.id == product.category_id,
                                     CategoryModel.is_active == True)
     )
-    category = temp_category.fist()
+    category = temp_category.first()
     if not category:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="Category not found or inactive")
@@ -114,7 +112,7 @@ async def update_product(product_id: int, product: ProductCreate, db: AsyncSessi
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
 
     # Проверяем, существует ли активная категория
-    temp_category = db.scalars(
+    temp_category = await db.scalars(
         select(CategoryModel).where(CategoryModel.id == product.category_id,
                                     CategoryModel.is_active == True)
     )
@@ -153,4 +151,4 @@ async def delete_product(product_id: int, db: AsyncSession = Depends(get_async_d
     product.is_active = False
     await db.commit()
 
-    return {"status": "success", "message": "Product marked as inactive"}
+    return product
